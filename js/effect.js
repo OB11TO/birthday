ca4c393e2c02b187ce974a3a981caaf2c5355bb2
OB11TO <ob11to61@gmail.com>
2023-09-18 17:52:50 +0300

    add video hb

--- js/effect.js
+++ js/effect.js
@@ -180,8 +180,8 @@ $('document').ready(function(){
 			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
 			i=i+1;
 			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
-			if(i==1){
-				$("p:nth-child(73)").fadeOut('slow').promise().done(function () {
+			if(i==74){
+				$("p:nth-child(74)").fadeOut('slow').promise().done(function () {
 					$('.cake').fadeIn('fast')
 					// После паузы аудио, показать кнопку и видео
 					$('.button-container').fadeIn('fast');
